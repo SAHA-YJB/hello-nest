b4d78d155f4386e790ac1e0352eec0f0ee4dff3c
SAHA-YJB <pkomom123@naver.com>
2025-07-25 13:52:30 +0900

    Movies API TEST init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
+// 데코레이터
+// app.module.ts 파일은 애플리케이션의 루트 모듈을 정의하는 파일
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  // url을 가져오고 함수를 실행하는 컨트롤러
+  // express의 라우터 역할
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
