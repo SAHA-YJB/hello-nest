927318d1ad0ec19acdca4f2f835c492d5da0fc10
SAHA-YJB <pkomom123@naver.com>
2025-07-25 14:23:49 +0900

    컨트롤러 내부 데코레이터 생성 및 테스트트

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 // @Controller('movies')의 괄호 안에 있는 문자열은 url의 엔트리포인트를 의미한다.
 // -> /movies
@@ -9,14 +9,21 @@ export class MoviesController {
     return 'This will return all movies';
   }
 
-  @Get('/:movieId')
+  // 쿼리 파라미터 사용
+  // 예) /movies/search?year=2025
+  @Get('search')
+  searchMovie(@Query('year') searchingYear: string): string {
+    return `We are searching for a movie made after: ${searchingYear}`;
+  }
+
+  @Get(':movieId')
   getOneMovie(@Param('movieId') movieId: string): string {
     return `This will return one movie with the id: ${movieId}`;
   }
 
   @Post()
-  createMovie(): string {
-    return 'This will create a movie';
+  createMovie(@Body() movieData: any): any {
+    return movieData;
   }
 
   @Delete('/:movieId')
@@ -25,7 +32,10 @@ export class MoviesController {
   }
 
   @Patch('/:movieId')
-  patchMovie(@Param('movieId') movieId: string): string {
-    return `This will patch a movie with the id: ${movieId}`;
+  patchMovie(@Param('movieId') movieId: string, @Body() updateData: any): any {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
   }
 }
